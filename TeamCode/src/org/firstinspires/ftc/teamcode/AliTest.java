0ebd78333ea2a4830f211d612198f1dfe6b84cf8
AlexandraNF <115591497+AlexandraNF@users.noreply.github.com>
2024-09-03 16:25:35 -0500

    added motors

--- TeamCode/src/org/firstinspires/ftc/teamcode/AliTest.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/AliTest.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="AliTest")
 
@@ -8,15 +10,34 @@ public class AliTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        double leftStickY = 0.0;
+
         int buttonPressCount = 0;
         boolean buttonPressed= false;
+
+        DcMotor front_left = hardwareMap.dcMotor.get("front_left_motor");
+        DcMotor front_right = hardwareMap.dcMotor.get("front_right_motor");
+        DcMotor back_left = hardwareMap.dcMotor.get("back_left_motor");
+        DcMotor back_right = hardwareMap.dcMotor.get("back_right_motor");
+
+        // Reverse the motors on the left side
+        front_left.setDirection(DcMotorSimple.Direction.REVERSE);
+        back_left.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            leftStickY = -gamepad1.left_stick_y;
+            double leftStickY = -gamepad1.left_stick_y;
+            double leftStickX = gamepad1.left_stick_x;
+            double triggers = gamepad1.left_trigger - gamepad1.right_trigger;
+
+            // Set the power on the motors
+            front_left.setPower(leftStickY + leftStickX - triggers);
+            front_right.setPower(leftStickY - leftStickX + triggers);
+            back_left.setPower(leftStickY - leftStickX - triggers);
+            back_right.setPower(leftStickY + leftStickX + triggers);
+
 
             /* if (gamepad1.a) {
                 buttonPressCount++;
